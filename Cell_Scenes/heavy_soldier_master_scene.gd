0666bfb5746ceb53a72a7387cb84cd7adeeec236
DonnyP-Augie <donaldpeters19@augustana.edu>
2024-01-27 16:46:07 -0600

    Learned and added some of the animations into one animation player to be used in a state machine

--- Cell_Scenes/heavy_soldier_master_scene.gd
+++ Cell_Scenes/heavy_soldier_master_scene.gd
@@ -19,7 +19,8 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	idle.play("mixamo_com")
+	idle.is_playing()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
