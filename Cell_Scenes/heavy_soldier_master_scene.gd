2f16a994efaddb88b7d4616bae8511037ef7f19b
DonnyP-Augie <donaldpeters19@augustana.edu>
2024-01-27 19:23:50 -0600

    Added collision meshes to the heavy soldier via its skeleton

--- Cell_Scenes/heavy_soldier_master_scene.gd
+++ Cell_Scenes/heavy_soldier_master_scene.gd
@@ -1,17 +1,5 @@
 extends Node3D
 
-@onready var idle_dying = $Idle_Dying
-@onready var reload_moving_forwards = $Reload_Moving_Forwards
-@onready var hit_reaction_idle = $Hit_Reaction_Idle
-@onready var turn_right_45_moving = $Turn_Right_45_Moving
-@onready var aiming_to_down = $Aiming_To_Down
-@onready var turn_right_90_not_aimed = $Turn_Right_90_Not_Aimed
-@onready var walking_hit_reaction = $Walking_Hit_Reaction
-@onready var down_to_aim = $Down_To_Aim
-@onready var start_walking_aimed = $Start_Walking_Aimed
-@onready var turn_left_45 = $Turn_Left_45
-@onready var turn_left_90 = $Turn_Left_90
-@onready var idle = $Idle
 
 @onready var line_of_sight = $Line_Of_Sight
 @onready var bullet_coords = $RootNode/AKM_scene/Bullet_Coords
@@ -19,8 +7,9 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	idle.play("mixamo_com")
-	idle.is_playing()
+	#idle.play("mixamo_com")
+	#idle.is_playing()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
